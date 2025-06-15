a7b6fef059d103a1aecf4a5803fd2786a9e2c0c9
dennis mbithi <denmbithi@gmail.com>
2025-06-15 18:50:57 +0300

    Updated totals updates

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const buttons = document.querySelectorAll(".fa-heart");
-  const carts = document.querySelectorAll(".card-body");
+  const carts = document.querySelectorAll(".product");
   const deleteButtons = document.querySelectorAll(".fa-trash-alt");
+  let total = parseInt(
+    document.querySelector(".total").textContent.replace(" $", "")
+  );
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -10,38 +13,42 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  let total = document.querySelector(".total");
-  let total_price = parseInt(total.textContent);
-
   carts.forEach((card) => {
-    const increase = card.querySelector(".fa-plus-circle");
-    const decrease = card.querySelector(".fa-minus-circle");
-    let quantity = parseInt(card.querySelector(".quantity").textContent);
     const price = parseInt(card.querySelector(".unit-price").textContent);
-    let amount = 0;
+    const decrease = card.querySelector(".fa-minus-circle");
+    const increase = card.querySelector(".fa-plus-circle");
+    console.log(total);
+    let quantity = parseInt(card.querySelector(".quantity").textContent);
 
     increase.addEventListener("click", () => {
       quantity++;
+      total += price;
       card.querySelector(".quantity").textContent = quantity;
-      amount = quantity * price;
+      document.querySelector(".total").textContent = `${total} $`;
     });
 
     decrease.addEventListener("click", () => {
       if (quantity > 0) {
         quantity--;
+        total -= price;
         card.querySelector(".quantity").textContent = quantity;
+        document.querySelector(".total").textContent = `${total} $`;
       }
-      amount = quantity * price;
     });
-
-    console.log(amount);
   });
 
   deleteButtons.forEach((deleteBtn) => {
     deleteBtn.addEventListener("click", () => {
       const item = deleteBtn.closest(".product");
-      console.log(item);
+      const price = parseInt(
+        item.querySelector(".unit-price").textContent.replace(" $", "")
+      );
+      const quantity = parseInt(item.querySelector(".quantity").textContent);
+
+      let amount = price * quantity;
       if (item) {
+        total -= amount;
+        document.querySelector(".total").textContent = `${total} $`;
         item.remove();
       }
     });
